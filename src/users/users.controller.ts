9801ca842320397aada53f116ce3e3b3397ff148
ALEKSANDR GOLIKOV <10749+goalrazor@users.noreply.github.yandex-team.ru>
2022-09-09 17:37:44 +0300

    roles

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,12 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Get, Post, UseGuards} from '@nestjs/common';
 import {CreateUserDto} from "./dto/create-user.dto";
 import {UsersService} from "./users.service";
 import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {User} from "./users.model";
+import {Roles} from "../auth/roles-auth.decorator";
+import {RolesGuard} from "../auth/roles.guard";
+import {AddRoleDto} from "./dto/add-role.dto";
+import {BanUserDto} from "./dto/ban-user.dto";
 
 @ApiTags('Пользователи')
 @Controller('users')
@@ -18,8 +22,28 @@ export class UsersController {
     }
     @ApiOperation({summary: 'Получить всех пользователей'})
     @ApiResponse({status:200, type: [User]})
+    @Roles('ADMIN')
+    @UseGuards(RolesGuard)
     @Get()
     getAll() {
         return this.userService.getAllUsers()
+    }
+
+    @ApiOperation({summary: 'Выдать роль'})
+    @ApiResponse({status:200})
+    @Roles('ADMIN')
+    @UseGuards(RolesGuard)
+    @Post('/role')
+    addRole(@Body() dto: AddRoleDto) {
+        return this.userService.addRole(dto)
+    }
+
+    @ApiOperation({summary: 'Забанить пользователя'})
+    @ApiResponse({status:200})
+    @Roles('ADMIN')
+    @UseGuards(RolesGuard)
+    @Post('/ban')
+    ban(@Body() dto: BanUserDto) {
+        return this.userService.ban(dto)
     }
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -13,23 +13,23 @@ interface IUserCreationAttrs {
 })
 export class User extends Model<User, IUserCreationAttrs> {
     @ApiProperty({example: '1', description: 'Уникальный идентификатор'})
-    @Column({type: DataType.INTEGER, unique:true, autoIncrement:true, primaryKey:true})
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
 
     @ApiProperty({example: 'user@mail.ru', description: 'Почта'})
-    @Column({type: DataType.STRING, unique:true, allowNull:false})
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
     email: string;
 
     @ApiProperty({example: '1234567dfgr№', description: 'Пароль пользователя'})
-    @Column({type: DataType.STRING, allowNull:false})
+    @Column({type: DataType.STRING, allowNull: false})
     password: string;
 
     @ApiProperty({example: 'true', description: 'Флаг забаненности'})
-    @Column({type: DataType.BOOLEAN, defaultValue:false})
+    @Column({type: DataType.BOOLEAN, defaultValue: false})
     banned: boolean;
 
     @ApiProperty({example: 'За хулиганство', description: 'Причина бана'})
-    @Column({type: DataType.STRING, allowNull:true})
+    @Column({type: DataType.STRING, allowNull: true})
     banReason: string;
 
     @BelongsToMany(() => Role, () => UserRoles)
